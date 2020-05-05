38be15346af187d0c38c92109ae808ffe5778f5c
Dario Sangiovanni <tenundertiber@gmail.com>
2020-05-05 10:46:57 +0200

    Version updated to 1.7.0 APK signing script update

--- scripts/signapk.sh
+++ scripts/signapk.sh
@@ -18,30 +18,70 @@ if [[ -z "$3" ]]; then
 	exit 1;
 fi
 
-APKFILE=$1
-KEYSTORE_PATH=$2
-KEY_ALIAS=$3
+if [[ ! -f "$1" ]]; then
+	echo "No APK file at ${1}";
+	exit 1;
+fi
+
+if [[ ! -f "$2" ]]; then
+	echo "No keystore file at ${2}";
+	exit 1;
+fi
+
 SLEEP=`which sleep`
 ZIP=`which zip`
 UNZIP=`which unzip`
 ZIPALIGN=`which zipalign`
 JARSIGNER=`which jarsigner`
-APKUNZIPDIR=`echo "${APKFILE%%.*}"`
-APKREZIPPED=`echo $APKFILE".rezipped"`
-APKFINAL=`echo $APKFILE".final"`
 CP=`which cp`
 RM=`which rm`
 MV=`which mv`
 BANNERNAME=emptybanner.png
 
+if [[ -z "$ZIP" ]]; then
+	echo "zip command not found";
+	exit 1;
+fi
 
+if [[ -z "$UNZIP" ]]; then
+	echo "unzip command not found";
+	exit 1;
+fi
 
+if [[ -z "$ZIPALIGN" ]]; then
+	echo "zipalign command not found";
+	exit 1;
+fi
+
+if [[ -z "$JARSIGNER" ]]; then
+	echo "jarsigner command not found";
+	exit 1;
+fi
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+APKFILE=`realpath "${1}"`
+KEYSTORE_PATH=`realpath "${2}"`
+KEY_ALIAS=$3
+APKUNZIPDIR=`echo "${APKFILE%%.*}"`
+APKREZIPPED=`echo $APKFILE".rezipped"`
+APKORIGINAL=`echo $APKFILE".original"`
+APKFINAL=`echo $APKFILE".final"`
+
+echo "APKFILE: ${APKFILE}"
+echo "KEYSTORE_PATH: ${KEYSTORE_PATH}"
+echo "KEY_ALIAS: ${KEY_ALIAS}"
+echo "APKUNZIPDIR: ${APKUNZIPDIR}"
+echo "APKREZIPPED: ${APKREZIPPED}"
+echo "APKORIGINAL: ${APKORIGINAL}"
+echo "APKFINAL: ${APKFINAL}"
+
+$CP $APKFILE $APKORIGINAL
 $UNZIP $APKFILE -d $APKUNZIPDIR
 
-# Uncomment this to rewrite banner
-# $CP $BANNERNAME $APKUNZIPDIR"/res/drawable/_corona_splash_screen.png"
-
-cd "$APKUNZIPDIR"
+cd $APKUNZIPDIR
 $CP assets/*.ogg res/raw
 $CP assets/*.wav res/raw
 #$RM assets/*.caf
@@ -49,12 +89,13 @@ $CP assets/*.wav res/raw
 #$RM assets/*.wav
 $ZIP -r $APKREZIPPED . *
 cd ..
-$RM -rf $APKUNZIPDIR
 $ZIP -d $APKREZIPPED META-INF/\*
 $JARSIGNER -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE_PATH $APKREZIPPED $KEY_ALIAS
 $JARSIGNER -verify -verbose -certs $APKREZIPPED
 $ZIPALIGN -v 4 $APKREZIPPED $APKFINAL
-$RM $APKREZIPPED
-$RM $APKFILE
+
+$RM -rf $APKUNZIPDIR
+$RM -f $APKREZIPPED
+$RM -f $APKFILE
 $MV $APKFINAL $APKFILE
 
